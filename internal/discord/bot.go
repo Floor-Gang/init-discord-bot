af248cb014fb0211f1ddaa41eda6549a7cbd45a3
Dylan Hackworth <dhpf@pm.me>
2020-07-20 12:14:41 -0500

    Added auth server

--- internal/discord/bot.go
+++ internal/discord/bot.go
@@ -1,32 +1,65 @@
 package discord
 
 import (
+	auth "github.com/Floor-Gang/authclient"
 	"github.com/Floor-Gang/init-discord-bot/internal"
 	util "github.com/Floor-Gang/utilpkg"
 	dg "github.com/bwmarrin/discordgo"
+	"log"
 )
 
 // Bot structure
 type Bot struct {
-	version string
-	session *dg.Session
-	config  *internal.Config
+	Auth   *auth.AuthClient
+	Client *dg.Session
+	Config *internal.Config
 }
 
 // Start starts discord client, configuration and database
-func Start(config internal.Config) error {
+func Start() {
 	var err error
-	botConfig := internal.GetConfig(config.Location)
 
-	client, err := dg.New("Bot " + botConfig.Token)
+	// get Config.yml
+	config := internal.GetConfig()
+
+	// setup authentication server
+	// you can use this to get the bot's access token
+	// and authenticate each user using a command.
+	authClient, err := auth.GetClient(config.Auth)
+
+	if err != nil {
+		log.Fatalln("Failed to connect to authentication server", err)
+	}
+
+	register, err := authClient.Register(
+		auth.Feature{
+			Name:        "", // Give this bot / feature a name
+			Description: "", // Describe what this bot is doing
+			Commands: []auth.SubCommand{ // list all the commands this bot / feature has
+				{
+					Name:        "",           // Command name like "add"
+					Description: "",           // Describe what the command does
+					Example:     []string{""}, // [command name, argument 1, argument 2] like [add, #channel, #channel]
+				},
+			},
+			CommandPrefix: config.Prefix,
+		},
+	)
+
+	if err != nil {
+		log.Fatalln("Failed to register with authentication server", err)
+	}
+
+	client, err := dg.New(register.Token)
 
 	if err != nil {
 		panic(err)
 	}
 
 	bot := Bot{
-		session: client,
-		config:  &botConfig,
+		Auth:   &authClient,
+		Client: client,
+		Config: &config,
 	}
 
 	client.AddHandler(bot.onReady)
@@ -35,6 +68,4 @@ func Start(config internal.Config) error {
 	if err = client.Open(); err != nil {
 		util.Report("Was an authentication token provided?", err)
 	}
-
-	return err
 }
